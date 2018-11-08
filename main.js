895dd458dd9f8fe0304f20f2024b980a59b33ca1
Zoe Oliveros <voliveros@fordham.edu>
2018-11-08 11:46:17 -0500

    change

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 
-$('button').click(function NewTask() {
-		var inputItem = document.getElementById("AddTask").value;
-		var i = document.createTextNode(inputItem);
-		var newItem = document.createElement("li");
-		newItem.appendChild(i);
+  $('button').click(function NewTask() {
+  		var inputItem = document.getElementById("AddTask").value;
+  		var i = document.createTextNode(inputItem);
+  		var newItem = document.createElement("li");
+  		newItem.appendChild(i);
 
-    document.getElementById("MyToDoList").appendChild(newItem);
-	});
-	});
+      document.getElementById("MyToDoList").appendChild(newItem);
+  	});
 
 
   	$('ol').on('click', 'li', function(){
       	$(this).hide("show");
       });
+
+});
